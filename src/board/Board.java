cd81a2b4bf3debda4b7d48e03e0d8b45fcba53ed
Joao <joaokavalciuki@hotmail.com>
2024-04-18 16:11:51 -0300

    Função: Adicionado BoardException e sua implentação no Board

--- src/board/Board.java
+++ src/board/Board.java
@@ -6,6 +6,10 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns < 1){
+            throw new BoardException("Error at board creating board! It must have at least one column and one line");
+        }
+
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
@@ -15,29 +19,50 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece getPiece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Position non existent");
+        }
+
         return pieces[row][column];
     }
 
     public Piece getPiece(Position position){
-        return pieces[position.getRow()][position.getRow()];
+        if(!positionExists(position)){
+            throw new BoardException("Position non existent");
+        }
+
+        return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("Position " + position + " alredy occupied");
+        }
+
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column){
+        return ((row >= 0 && row <= rows) && (column >= 0 && column <= columns));
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position non existent");
+        }
+
+        return getPiece(position) != null;
     }
 
 }
