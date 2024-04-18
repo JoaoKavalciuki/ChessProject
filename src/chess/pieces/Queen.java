ec4c2cfd66b90a7d269031677bb377a896c1547c
Joao <joaokavalciuki@hotmail.com>
2024-04-18 14:36:53 -0300

    Função Peças de xadrez criadas

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,16 @@
+package chess.pieces;
+
+import board.Board;
+import chess.ChessPiece;
+import chess.enums.Color;
+
+public class Queen extends ChessPiece {
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+}
