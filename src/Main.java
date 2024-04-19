21843f5cd5e4172eafb9c02239f79f7175f82eef
Joao <joaokavalciuki@hotmail.com>
2024-04-19 12:38:15 -0300

    Função Imprimindo o tabuleiro para testes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        ChessMatch match = new ChessMatch();
+
+
+        while(true){
+            try{
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+
+                System.out.print("Source: ");
+                ChessPosition source = UI.readPosition(sc);
+
+                System.out.print("Target: ");
+                ChessPosition target = UI.readPosition(sc);
+
+                ChessPiece captturedPiece = match.performNewMove(source, target);
+
+            } catch(ChessException | InputMismatchException exception){
+                System.out.println(exception.getMessage());
+                sc.nextLine();
+            }
+        }
+
     }
 }
