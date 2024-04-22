ad6d95deda57ad1dbd20a51be609ca2f15ac4f1a
Joao <joaokavalciuki@hotmail.com>
2024-04-22 19:27:30 -0300

    Função: Movimentação bispo implementada

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -12,5 +13,71 @@ public class Bishop extends ChessPiece {
     @Override
     public String toString() {
         return "B";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] auxMatrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+        //NW
+        auxPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+
+        while(getBoard().positionExists(auxPosition) &&  !getBoard().thereIsAPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+            auxPosition.setRow(auxPosition.getRow()-1);
+            auxPosition.setColumn(auxPosition.getColumn()-1);
+        }
+
+        if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //NE
+        auxPosition.setValues(position.getRow()-1, position.getColumn()+1);
+
+        while(getBoard().positionExists(auxPosition) &&  !getBoard().thereIsAPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+            auxPosition.setColumn(auxPosition.getColumn()+1);
+            auxPosition.setRow(auxPosition.getRow()-1);
+        }
+
+        if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //SW
+        auxPosition.setValues(position.getRow()+1, position.getColumn()-1);
+
+        while(getBoard().positionExists(auxPosition) &&  !getBoard().thereIsAPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+            auxPosition.setColumn(auxPosition.getColumn()-1);
+            auxPosition.setRow(auxPosition.getRow()+1);
+        }
+
+        if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //SE
+        auxPosition.setValues(position.getRow()+1, position.getColumn()+1);
+
+        while(getBoard().positionExists(auxPosition) &&  !getBoard().thereIsAPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+            auxPosition.setRow(auxPosition.getRow()+1);
+            auxPosition.setColumn(auxPosition.getColumn()+1);
+        }
+
+        if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+
+        return auxMatrix;
     }
 }
