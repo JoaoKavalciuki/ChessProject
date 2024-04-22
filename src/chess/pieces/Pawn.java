b0940acfb61968063a616266b65e7a9ab7a5b734
Joao <joaokavalciuki@hotmail.com>
2024-04-22 18:59:30 -0300

    Função: Movimentos do peão implementado

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,6 +1,8 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
+import chess.ChessException;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -12,5 +14,72 @@ public class Pawn extends ChessPiece {
     @Override
     public String toString() {
         return "P";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] auxMatrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+        if(getColor() == Color.WHITE){
+            auxPosition.setValues(position.getRow()-1, position.getColumn());
+            if(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+
+            auxPosition.setValues(position.getRow()-2, position.getColumn());
+            Position targetPostion = new Position(position.getRow()-1, position.getColumn());
+
+            if(!getBoard().positionExists(targetPostion) || getBoard().thereIsAPiece(targetPostion)){
+                throw new ChessException("There is alredy a piece at target position or it does not exists");
+            }
+
+            if(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition) && this.getMoveCount() == 0){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+
+
+            //diagonal esquerda
+            auxPosition.setValues(position.getRow()-1, position.getColumn()-1);
+            if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+
+            //diagonal direita
+            auxPosition.setValues(position.getRow()-1, position.getColumn()+1);
+            if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+        } else {
+            auxPosition.setValues(position.getRow()+1, position.getColumn());
+            if(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+
+            auxPosition.setValues(position.getRow()+2, position.getColumn());
+            Position targetPostion = new Position(position.getRow()+1, position.getColumn());
+
+            if(!getBoard().positionExists(targetPostion) || getBoard().thereIsAPiece(targetPostion)){
+                throw new ChessException("There is alredy a piece at target position or it does not exists");
+            }
+
+            if(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition) && this.getMoveCount() == 0){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+
+
+            //diagonal esquerda
+            auxPosition.setValues(position.getRow()+1, position.getColumn()-1);
+            if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+            auxPosition.setValues(position.getRow()+1, position.getColumn()+1);
+            if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)){
+                auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+            }
+        }
+
+        return  auxMatrix;
     }
 }
