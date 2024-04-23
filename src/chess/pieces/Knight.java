9c3d1de720923ff5f369522092323d7ced86d82f
Joao <joaokavalciuki@hotmail.com>
2024-04-23 10:45:37 -0300

    Função: Movimentação do Cavalo implementada

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -12,5 +13,69 @@ public class Knight extends ChessPiece {
     @Override
     public String toString() {
         return "N";
+    }
+
+
+    private boolean canMove(Position position){
+        ChessPiece pieceAtPosition = (ChessPiece) getBoard().getPiece(position);
+        return (pieceAtPosition == null || pieceAtPosition.getColor() != this.getColor());
+    }
+
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] auxMatrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+
+        //acima
+        auxPosition.setValues(position.getRow()-1, position.getColumn() -2);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //direita
+        auxPosition.setValues(position.getRow() -2, position.getColumn() -1);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //abaixo
+        auxPosition.setValues(position.getRow()-2, position.getColumn()+1);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+        //esquerda
+        auxPosition.setValues(position.getRow()-1, position.getColumn()+2);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //diagonal esquerda cima
+        auxPosition.setValues(position.getRow()+1, position.getColumn()+2);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //diagona direita cima
+
+        auxPosition.setValues(position.getRow()+2, position.getColumn()+1);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        //diagonal esquerda baixo
+        auxPosition.setValues(position.getRow()+2, position.getColumn()-1);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        auxPosition.setValues(position.getRow()+1, position.getColumn()-2);
+        if(getBoard().positionExists(auxPosition) && canMove(auxPosition)){
+            auxMatrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        return auxMatrix;
     }
 }
